fc935f240c4dae031901254418294e8af3f17c7c
Daniel Marques <danielfmarques@ua.pt>
2020-04-19 22:38:23 +0100

    DataSiren project

---
+++ DataSirenWebApp/src/main/java/com/springKafka/datasiren/datasirenApplication.java
@@ -0,0 +1,18 @@
+package com.springKafka.datasiren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.springKafka")
+public class datasirenApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(datasirenApplication.class, args);
+		
+	}
+}
