e84c794ff64d87bcf64748bc0cad457b7b146589
RaquelRainho <raquel.a.rainho@ua.pt>
2020-04-20 01:59:46 +0100

    file location readjustment

---
+++ Jenkinsfile
@@ -0,0 +1,16 @@
+pipeline {
+	agent any
+
+	stages{
+            stage('Build'){
+                steps{
+                    sh 'echo "trying to build"'
+                }
+            }
+            stage('Deploy'){
+                steps{
+                    sh 'echo "trying to deploy"'
+                }
+            }
+	}
+}
---
+++ src/test/java/com/springKafka/datasiren/datasirenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springKafka.datasiren;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class datasirenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
